8de9b1bf7af4138ebc24ddd10516f04414eba22e
Quoc-V <viet.nguyen@rwth-aachen.de>
2022-01-10 13:20:51 +0100

    added menue and video integration

--- scripts/main.js
+++ scripts/main.js
@@ -1,8 +1,15 @@
 function changeImage() {
-	let img = document.getElementById("Banner");
+	var img = document.getElementById("Banner");
+	var vid = document.getElementById("Cobra_Video")
 	if (img.src.match("GoT")) {
 		img.src = "images/Cobra.jpg"
+		vid.src = "https://www.youtube.com/embed/x8eRMQtjCBE"
 		alert('This dojo has been taken over by COBRA KAI')
 		}
+	else {
+		img.src = "images/GoT_Banners.jpg"
+		alert('The great houses of Westeros have succesfully reclaimed their dominion.')
+	}
+
 	
 }
