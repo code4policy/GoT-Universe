d1d1d05660019158ec7be3b4b8420fed47d69c66
llewrubnala <alanburwell@hks.harvard.edu>
2022-01-10 08:51:19 -0500

    add js mouseover text update

---
+++ scripts/main.js
@@ -0,0 +1,56 @@
+
+
+
+
+function update_house_text_1() {
+
+	document.getElementById("house_words").innerHTML = "House Stark";
+
+}
+
+function update_house_text_2() {
+
+	document.getElementById("house_words").innerHTML = "House something";
+
+}
+
+function update_house_text_3() {
+
+	document.getElementById("house_words").innerHTML = "House Stark";
+
+}
+
+function update_house_text_4() {
+
+	document.getElementById("house_words").innerHTML = "House something";
+
+}
+function update_house_text_5() {
+
+	document.getElementById("house_words").innerHTML = "House Stark";
+
+}
+
+function update_house_text_6() {
+
+	document.getElementById("house_words").innerHTML = "House something";
+
+}
+
+function update_house_text_7() {
+
+	document.getElementById("house_words").innerHTML = "House Stark";
+
+}
+
+function update_house_text_8() {
+
+	document.getElementById("house_words").innerHTML = "House something";
+
+}
+
+function update_house_text_9() {
+
+	document.getElementById("house_words").innerHTML = "House Stark";
+
+}
